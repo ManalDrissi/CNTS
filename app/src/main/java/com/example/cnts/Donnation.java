06f24edae95b2ff0cf181e11f86ccda7c295830e
Aymen <aymen@alphorm.com>
2021-04-27 01:46:15 +0000

    Add donations shit

---
+++ app/src/main/java/com/example/cnts/Donnation.java
@@ -0,0 +1,31 @@
+package com.example.cnts;
+
+public class Donnation {
+    private String center;
+    private String date;
+    private String time;
+
+    public void setCenter(String center) {
+        this.center = center;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCenter() {
+        return center;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
